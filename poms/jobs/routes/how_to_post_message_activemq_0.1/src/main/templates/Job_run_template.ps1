21dd25104e56c82dd9686807bedb01d95fb516b0
smitha <smitha.kerenalli@gmail.com>
2020-12-23 18:25:27 -0800

    Adding initial project

---
+++ poms/jobs/routes/how_to_post_message_activemq_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/beans.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/camel-core-2.24.2.jar;../lib/camel-spring-2.24.2.jar;../lib/spring-beans-5.1.4.RELEASE.jar;../lib/spring-context-5.1.4.RELEASE.jar;../lib/spring-core-5.1.4.RELEASE.jar;../lib/crypto-utils.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;how_to_post_message_activemq_0_1.jar;' local_project.how_to_post_message_activemq_0_1.how_to_post_message_ActiveMQ  --context=Default $args
---
+++ poms/jobs/routes/how_to_post_message_activemq_0.1/src/main/templates/Job_run_template.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd `dirname $0`
+ROOT_PATH=`pwd`
+java -Dtalend.component.manager.m2.repository=$ROOT_PATH/../lib -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/beans.jar:$ROOT_PATH/../lib/log4j-slf4j-impl-2.12.1.jar:$ROOT_PATH/../lib/log4j-api-2.12.1.jar:$ROOT_PATH/../lib/log4j-core-2.12.1.jar:$ROOT_PATH/../lib/camel-core-2.24.2.jar:$ROOT_PATH/../lib/camel-spring-2.24.2.jar:$ROOT_PATH/../lib/spring-beans-5.1.4.RELEASE.jar:$ROOT_PATH/../lib/spring-context-5.1.4.RELEASE.jar:$ROOT_PATH/../lib/spring-core-5.1.4.RELEASE.jar:$ROOT_PATH/../lib/crypto-utils.jar:$ROOT_PATH/../lib/slf4j-api-1.7.25.jar:$ROOT_PATH/../lib/dom4j-2.1.1.jar:$ROOT_PATH/how_to_post_message_activemq_0_1.jar: local_project.how_to_post_message_activemq_0_1.how_to_post_message_ActiveMQ  --context=Default "$@"
